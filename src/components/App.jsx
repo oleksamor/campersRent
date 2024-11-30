02236ce7fa059506a91f44e3a7b699aeb449b359
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-11-30 22:05:51 +0200

    udd routers, notfound

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Home/Home.jsx";
+import NotFound from "./NotFound/NotFound.jsx";
 
 function App() {
   return (
-    <>
-      <Route path="/" element={<Home />} />
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/campers" element={<CatalogPage />} />
+        <Route path="/campers/:id" element={<CamperId />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
-import { Provider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
