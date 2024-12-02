0d24a541d21de0f15bafc2301c1af2d88f135ff6
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-12-02 10:03:27 +0200

    update main.jsx, Catalog, add import Provider, store,

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -15,7 +15,34 @@ const Catalog = () => {
 
   if (loading) return <p>Loading...</p>;
 
-  return;
+  return (
+    <div className="containerCatalog">
+      <div className="{css.containerFilter}">
+        <form>
+          <div>
+            <label htmlFor="location">Location</label>
+            <input type="checkbox" />
+          </div>
+
+          <div>
+            <label htmlFor="filters">Filters</label>
+            <h3>Vehicle equipment</h3>
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <h3>Vehicle type</h3>
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+          </div>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Catalog;
