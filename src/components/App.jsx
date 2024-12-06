192bf8c48edf443bf2cc32bb10860cc68dc19847
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-12-06 10:08:07 +0200

    moving folders Home, NotFound, Catalog, SelectedCamper

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./Home/Home.jsx";
+import Home from "../page/Home/Home.jsx";
 import NotFound from "./NotFound/NotFound.jsx";
 import Header from "./Header/Header.jsx";
 // import Catalog from "./Catalog/Catalog.jsx";
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/campers" element={<Catalog />} /> */}
-        {/* <Route path="/campers/:id" element={<CamperId />} /> */}
+        {/* <Route path="/campers/:id" element={<SelectedCamper />} /> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
---
+++ src/page/SelectedCamper/SelectedCamper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectedCamper = () => {
+  return <div>SelectedCamper</div>;
+};
+
+export default SelectedCamper;
